cfafdb8ad3ec479299d777fcaaf9936ea4bac1a8
Werner Schneider <werner@schneidernet.com>
2017-01-20 09:48:44 +0100

    Motor geht jetzt, dafür der ADC noch nicht

--- src/bot.cpp
+++ src/bot.cpp
@@ -1,3 +1,4 @@
+#include "MCP3008.h"
 #include "MyLcd.h"
 #include <Arduino.h>
 
@@ -5,9 +6,11 @@
 #define BUTN 10
 
 // LED's signalize the CNY70 focus
-#define LED1 0
-#define LED2 1
-#define LED3 2
+#define LED1 17
+#define LED2 18
+#define LED3 19
+#define LED_GREEN 20
+#define LED_RED 21
 
 // LCD
 #define LCD_RS 1
@@ -21,29 +24,37 @@
 #define SHIFT_CLOCK 5 // CLK -> PD5
 #define SHIFT_DATA 6  // DATA -> PD6
 #define SHIFT_LATCH 9 // ENABLE ->PB1
+#define SHIFT_REGISTER 3
 
 // MOTOR
-#define MOTOR_PIN_OFFSET
+#define MOTOR1_PIN_OFFSET 8
+#define MOTOR2_PIN_OFFSET 12
+
+// ADC
+// man könnte die Output Pins auch über das Schieberegister schalten
+// aber dann dauert eine Abfrage länger
+#define ADC_CS_PIN 11
+#define ADC_CLOCK_PIN 16
+#define ADC_MOSI_PIN 17
+#define ADC_MISO_PIN 18
 
 #define SENSOR1 17 // SENSOR1
 #define SENSOR2 18 // SENSOR2
 #define SENSOR3 19 // SENSOR3
 
-#define SHIFT_REGISTER 3
-
 // Global Vars
 int leds[] = {LED1, LED2, LED3};
 MyLcd *lcd;
+MCP3008 *adc;
 
 // Motor at default off
 int motor_on = false;
 
-// Intervall of sensor-reads / motor-cycles
-#define INTV 10
-int iv = INTV;
-
-// motor direction
-int dir = 0;
+// Motor Schritt-Weite - das ist die Zeit, zwischen den einzelnen Steps (z.B.
+// Zeit
+// bis der Magnet wirkt - mind 2000) - das regelt die Geschwindigkeit
+int w_l = 3000;
+int w_r = 6000;
 
 // button - changed state
 int changed = false;
@@ -52,7 +63,7 @@ int changed = false;
 set state of Pins
 */
 volatile uint32_t data = 0;
-static void writeBit(uint8_t b) {
+static void writeByte(uint8_t b) {
   asm volatile("ldi   r17, 8\n" // 8 bits to work
                "next_bit:\n"
                "sbrs  %0,7\n"   // skip if bit 7 is set
@@ -74,9 +85,9 @@ void shiftWrite() {
   asm volatile("cli\n"          // Disable Interrupts
                "cbi 0x05,1\n"   // Port B1 Low
                "cbi 0x0b,5\n"); // PORT D5 Low (Clk initial Low)
-  writeBit(((char *)&data)[2]);
-  writeBit(((char *)&data)[1]);
-  writeBit(((char *)&data)[0]);
+  writeByte(((char *)&data)[2]);
+  writeByte(((char *)&data)[1]);
+  writeByte(((char *)&data)[0]);
   asm volatile("sbi 0x05,1\n" // Port B1 High
                "sei\n");      // Enable Interrupts
 }
@@ -111,40 +122,12 @@ void setup(void) {
 
   setup_pins();
 
+  lcd = new MyLcd(LCD_RS, LCD_ENABLE, LCD_D4, LCD_D5, LCD_D6, LCD_D7,
+                  &digitalWritePin);
+
+  adc = new MCP3008(ADC_CLOCK_PIN, ADC_MOSI_PIN, ADC_MISO_PIN, ADC_CS_PIN);
+
   // TODO: Calibrate Sensors
-}
-
-/*
-drives a motor
-*/
-void driveMotor(int motor, int d) {
-
-  // if no delay -> motor stop
-  if (!d)
-    return;
-
-  // select motor and do 1 wave clycle
-  int spin = motor * 4;
-  for (int i = spin; i < 4 + spin; i++) {
-    digitalWritePin(MOTOR_PIN_OFFSET + i, HIGH);
-    delay(d);
-    digitalWritePin(MOTOR_PIN_OFFSET + i, LOW);
-  }
-}
-
-void driveMotors(int val) {
-
-  // adjust value to array
-  val += 3;
-
-  // speed table
-  int w1[] = {0, 10, 5, 2, 2, 2, 2};
-  int w2[] = {2, 2, 2, 2, 5, 10, 0};
-
-  if (motor_on) {
-    driveMotor(0, w1[val]);
-    driveMotor(1, w2[val]);
-  }
 }
 
 void clearLeds() {
@@ -153,7 +136,7 @@ void clearLeds() {
   }
 }
 
-int readSensor(int sensor) { return analogRead(sensor); }
+int readSensor(int sensor) { return adc->readADC(sensor); }
 
 int readPrintSensors(int sensor, int x) {
   int value = readSensor(sensor);
@@ -162,76 +145,132 @@ int readPrintSensors(int sensor, int x) {
   return value;
 }
 
+// Schritt-Sequenz für motor
+// Da gibts noch ein Muster, dass mehr Kraft auf den Motor ausübt
+void motorStep(int offset, int thisStep) {
+  switch (thisStep) {
+  case 0:
+    data |= (1L << (offset + 0));
+    data &= ~(1L << (offset + 1));
+    data &= ~(1L << (offset + 2));
+    data &= ~(1L << (offset + 3));
+    break;
+  case 1:
+    data &= ~(1L << (offset + 0));
+    data |= (1L << (offset + 1));
+    data &= ~(1L << (offset + 2));
+    data &= ~(1L << (offset + 3));
+    break;
+  case 2:
+    data &= ~(1L << (offset + 0));
+    data &= ~(1L << (offset + 1));
+    data |= (1L << (offset + 2));
+    data &= ~(1L << (offset + 3));
+    break;
+  case 3:
+    data &= ~(1L << (offset + 0));
+    data &= ~(1L << (offset + 1));
+    data &= ~(1L << (offset + 2));
+    data |= (1L << (offset + 3));
+    break;
+  }
+  shiftWrite();
+}
+
 /*
-Main Loop
+Steuert den Motor
 */
-void loop(void) {
+void motorLoop(void (*f)()) {
+  uint32_t next_l = micros() + w_l;
+  uint32_t next_r = micros() + w_r;
 
-  lcd = new MyLcd(LCD_RS, LCD_ENABLE, LCD_D4, LCD_D5, LCD_D6, LCD_D7,
-                  &digitalWritePin);
-  lcd->begin(20, 4);
-
-  int count = 0;
-  lcd->setCursor(0, 0);
-  lcd->print("Hello World");
+  int step_l = 0;
+  int step_r = 0;
   while (true) {
-
-    lcd->setCursor(12, 0);
-    lcd->print(count++);
-    delay(500);
-    driveMotors(1);
-  }
-
-  if (iv-- == 0) {
-    iv = INTV;
-
-    clearLeds();
-
-    lcd->clear();
-    int s1 = readPrintSensors(0, 0);
-    int s2 = readPrintSensors(1, 4);
-    int s3 = readPrintSensors(2, 8);
-
-    if (s1 < s2) {
-      if (s1 < s3) {
-        //    shiftWrite(LED1, HIGH);
-        if (dir > -3)
-          dir--;
-      } else {
-        //  shiftWrite(LED3, HIGH);
-        if (dir < 3)
-          dir++;
-      }
-    } else {
-      if (s2 < s3) {
-        // shiftWrite(LED2, HIGH);
-        dir = 0;
-      } else {
-        //        shiftWrite(LED3, HIGH);
-        if (dir < 3)
-          dir++;
-      }
+    if (micros() > next_l) {
+      if (motor_on)
+        motorStep(MOTOR1_PIN_OFFSET, step_l++);
+      if (step_l > 3)
+        step_l = 0;
+      next_l += w_l;
     }
-    if (!motor_on)
-      delay(50);
+    if (micros() > next_r) {
+      if (motor_on)
+        motorStep(MOTOR2_PIN_OFFSET, step_r--);
+      if (step_r < 0)
+        step_r = 3;
+      next_r += w_r;
+    }
+    f();
   }
+}
 
+// diese Funktion wird aufgerufen, sobald der Motor alle aktuell anfallenden
+// Schritte gemacht hat.
+void sensorLoop() {
+
+  // if (iv-- == 0) {
+  //   iv = INTV;
+  //
+  //   clearLeds();
+  //
+  //   lcd->clear();
+  //   int s1 = readPrintSensors(0, 0);
+  //   int s2 = readPrintSensors(1, 4);
+  //   int s3 = readPrintSensors(2, 8);
+  //
+  //   if (s1 < s2) {
+  //     if (s1 < s3) {
+  //       //    shiftWrite(LED1, HIGH);
+  //       if (dir > -3)
+  //         dir--;
+  //     } else {
+  //       //  shiftWrite(LED3, HIGH);
+  //       if (dir < 3)
+  //         dir++;
+  //     }
+  //   } else {
+  //     if (s2 < s3) {
+  //       // shiftWrite(LED2, HIGH);
+  //       dir = 0;
+  //     } else {
+  //       //        shiftWrite(LED3, HIGH);
+  //       if (dir < 3)
+  //         dir++;
+  //     }
+  //   }
+  //   if (!motor_on)
+  //     delay(50);
+  // }
+  //
   int btn = digitalRead(BUTN);
-  //  lcd.setCursor(12, 0);
-  //  lcd.print(btn);
+  lcd->setCursor(0, 0);
+  lcd->print(btn);
 
   if (!btn) {
     if (!changed) {
       motor_on = !motor_on;
       changed = true;
     }
-    //  lcd.setCursor(13, 0);
-    //  lcd.print(motor_on);
+    lcd->setCursor(1, 0);
+    lcd->print(motor_on);
   } else {
     changed = false;
   }
+}
 
-  //  lcd.setCursor(15, 0);
-  //  lcd.print(dir);
-  driveMotors(dir);
+/*
+Main Loop
+*/
+void loop(void) {
+
+  lcd->begin(20, 4);
+
+  // motorLoop(&sensorLoop);
+
+  while (true) {
+    lcd->setCursor(0, 0);
+    lcd->print(readSensor(0));
+    delay(100);
+  }
 }
